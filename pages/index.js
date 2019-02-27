048ca1bae60e32dd42699c38a1e45468d8402e79
Abrehamgit <gezahegnabereham@gmail.com>
2019-02-27 10:58:16 -0800

    Initial commit. Navbar set up some components added

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+const Index = (props) => (
+	<div> 
+		<h4> This is homeiee </h4>
+	</div>
+)
+
+
+export default Index;
---
+++ pages/sell.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+const Sell = (props) => (
+	<div> 
+		<h4> This is sell page </h4>
+	</div>
+)
+
+
+export default Sell;
